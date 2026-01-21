ceb737e304b5620be85d6b5efeb36174ee8cb99a
masaruichikawa <micontact91@gmail.com>
2026-01-21 22:25:23 +0900

    音のループ再生実装

--- Assets/Scripts/TypeWriter.cs
+++ Assets/Scripts/TypeWriter.cs
@@ -7,7 +7,9 @@ public class TypeWriter : MonoBehaviour
 {
     // public TMP_Text textUI;
     [SerializeField] public TMP_Text textUI;
-    public float interval = 0.5f;
+    public float interval = 0.002f;
+    public Dictionary<int, string> storySoundsWithText = new Dictionary<int, string>();
+    public string storySoundsData = "";
 
     public void StartTyping(string text)
     {
@@ -19,11 +21,16 @@ public class TypeWriter : MonoBehaviour
     IEnumerator TypeRoutine(string message)
     {
         textUI.text = "";
+        Debug.Log("ストーリーテキストの音データ"+storySoundsData);
+        // SoundManeger.Instance.Play(storySoundsData);
         foreach (char c in message)
         {
+          
             Debug.Log("タイプライターで文字を表示中:" + c);
             textUI.text += c;
-            yield return new WaitForSeconds(interval);
+            SoundManeger.Instance.Play("カーソル移動2");
+            yield return null;
+            // yield return new WaitForSeconds(interval);
         }
     }
 }
